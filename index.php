af4ad198acf3a9c5ef0aa7d87d7dddcd62bb3900
Amirali <liamirali.lotfi@gmail.com>
2021-05-22 12:19:25 +0430

    Add auto table generation

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>Emotion Tracker</title>
 	<link rel="stylesheet" type="text/css" href="css/nav.css">
 	<link rel="stylesheet" type="text/css" href="css/questionnaire.css">
-	<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="lib/jquery-3.6.0.slim.min.js"></script>
 </head>
 <body>
 	<nav>
@@ -13,7 +13,6 @@
 			<?php
 				$today = getdate()["mday"] ."/". getdate()["mon"] ."/". getdate()["year"];
 			?>
-			
 			<form action="#">
 				<lable for="date-input">date:</lable>
 				<input type="text" id="date-input" name="date" value="<?= $today ?>">
@@ -22,26 +21,32 @@
 	</nav>
 	<main>
 		<form action="#" method="POST">
-			<div id="questionnaire" class="q-grid-container">
+			<div id="qtables" class="q-grid-container">
 				<?php
-					$grid_content = [
-						["", "0 (not at all)", "1 (a little)", "2 (moderately)", "3 (quite a bit)", "4 (extremely)"],
-						["Anger-Hostility" , "", "", "", "", ""],
-						["Confusion-Bewilderment" , "", "", "", "", ""],
-						["Depression-Dejection" , "", "", "", "", ""],
-						["Fatigue-Inertia" , "", "", "", "", ""],
-						["Tension-Anxiety" , "", "", "", "", ""],
-						["Vigor-Activity" , "", "", "", "", ""],
-						["Friendliness" , "", "", "", "", ""]
-					];
-					for ($row = 0; $row < 8; $row++) {
-						for ($clm = 0; $clm < 6; $clm++) {
-							echo '<div id="'.$row.'-'.$clm.'"><p>'.$grid_content[$row][$clm].'</p>';
-							if (!($row == 0 || $clm == 0)) {
-								echo '<input required="required" type="radio" name="attr'.$row.'"'.' value="'.($clm-1).'">';
+					$table_config = include('config/qtable-config.php');
+					$num = 0;
+					for ($i = 0; $i < $table_config['table_count']; $i++) {
+						echo '<div class="qtable">';
+						for ($row = 0; $row < count($table_config['table_'.$i]); $row++) {
+							for ($clm = 0; $clm < count($table_config['table_'.$i][0]); $clm++) {
+								$cell_content = $table_config['table_'.$i][$row][$clm];
+								echo '<div class="qtable-item">';
+								$id = $i.'-'.$row.'-'.$clm;
+								if ($cell_content == "[RADIO]") {
+									$name = $i.'-'.$row;
+									echo '<input type="radio" class="qtable-radio" name="'.$name.'"  id='.$id.' value="'.$clm.'">';
+								} elseif (gettype($cell_content) == 'string' && strlen($cell_content) > 0) {
+									// it has to be a the name of the row
+									echo '<p>'.$table_config['table_'.$i][$row][$clm].'</p>';
+								} elseif ($cell_content == '[EMPTY]') {
+									continue;
+								} else {
+									echo "Undefined item type";
+								}
+								echo '</div>';
 							}
-							echo '</div>';
 						}
+						echo '</div>';
 					}
 				?>
 			</div>
@@ -51,6 +56,11 @@
 			</div>
 			<input id="submit-btn" type="submit" name="submit" value="Submit">
 		</form>
+		<?php
+			if ($_POST['submit'] == 'Submit') {
+				print_r($_POST);
+			}
+		?>
 	</main>
 </body>
 	<script type="text/javascript" src="js/style.js"></script>
