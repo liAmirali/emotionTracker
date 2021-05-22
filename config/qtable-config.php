593d4382f64ba41b77c2609aab9712face85479f
Amirali <liamirali.lotfi@gmail.com>
2021-05-22 12:18:39 +0430

    Add config file

---
+++ config/qtable-config.php
@@ -0,0 +1,24 @@
+<?php
+	return array(
+		'table_count' => 2,
+		'table_0' => [
+			["[EMPTY]", "0 (not at all)", "1 (a little)", "2 (moderately)", "3 (quite a bit)", "4 (extremely)"],
+			["Anger-Hostility", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]"],
+			["Confusion-Bewilderment", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]"],
+			["Depression-Dejection", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]"],
+			["Fatigue-Inertia", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]"],
+			["Tension-Anxiety", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]"],
+			["Vigor-Activity", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]"],
+			["Friendliness", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]"]
+		],
+		'table_1' => [
+			["[EMPTY]", "0 (not at all)", "1 (a little)", "2 (moderately)", "3 (quite a bit)", "4 (extremely)"],
+			["Elated-Depressed", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]"],
+			["Composed-Anxious", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]"],
+			["Confident-Unsure", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]"],
+			["Agreeable-Hostile", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]"],
+			["Clearheaded-Confused", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]"],
+			["Energetic-Tired", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]", "[RADIO]"]			
+		],
+	);
+?>
