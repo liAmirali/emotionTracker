7521f351b58251116adb510132f73da88d04b3e2
Amirali <liamirali.lotfi@gmail.com>
2021-05-29 14:14:09 +0430

    Merge config files

---
+++ config/cnf.php
@@ -0,0 +1,14 @@
+<?php
+	// Database configs
+	$servername = "localhost";
+	$username = "amirali";
+	$password = "@rn33rA1ee";
+	$dbname = "emotiontracker";
+
+
+	// Constants
+	$siteroot = "http://localhost/emotion";
+
+	// Time zone
+	date_default_timezone_set('Asia/Tehran');
+?>
