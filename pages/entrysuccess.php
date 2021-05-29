c30de49b98f85eda79061ac40b1a4e42c4b85b34
Amirali <liamirali.lotfi@gmail.com>
2021-05-29 14:13:41 +0430

    Better looks for entrysuccess page

--- pages/entrysuccess.php
+++ pages/entrysuccess.php
@@ -2,67 +2,78 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>HOORAY!</title>
+	<title>Emotion Tracker - Record Entry</title>
+
+	<link rel="stylesheet" type="text/css" href="../lib/bootstrap-4.6.0/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/navbar.css">
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
 </head>
 <body>
-	<center><h1>YOU DID IT! WOW!</h1></center>
 	<?php
-		include('../config/sqldb_conf.php');
+		include('../config/cnf.php');
+		include("../site/navbar.php");
+
 		$table_config = include('../config/qtable-conf.php');
 
-		// Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
+		if ($_POST['submit'] == "Submit") {
+		
+			// Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
 
-		// Check connection
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		}
-
-		$query = "CREATE TABLE IF NOT EXISTS entry (
-			id INT PRIMARY KEY AUTO_INCREMENT,
-			entered_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-			table_0 VARCHAR(15) NOT NULL,
-			table_1 VARCHAR(15) NOT NULL,
-			description VARCHAR(300)
-		)";
-
-		if ($conn->query($query) === TRUE) {
-			$desc = $_POST['description'];
-			$qtable_0 = "";
-			$qtable_1 = "";
-			$index = 0;
-			foreach ($_POST as $key => $value) {
-				if ($index < count($table_config['table_0']) - 1) {
-					$qtable_0 .= $value;
-				}
-				elseif ($index < count($table_config['table_0']) + count($table_config['table_1']) - 2) {
-					$qtable_1 .= $value;
-				}
-				$index++;
+			// Check connection
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
 			}
 
-			$query = "INSERT INTO entry (
-				table_0,
-				table_1,
-				description
-			)
-			VALUES (
-				'$qtable_0',
-				'$qtable_1',
-				'$desc'
+			$query = "CREATE TABLE IF NOT EXISTS entry (
+				id INT PRIMARY KEY AUTO_INCREMENT,
+				entered_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+				table_0 VARCHAR(15) NOT NULL,
+				table_1 VARCHAR(15) NOT NULL,
+				description VARCHAR(300)
 			)";
 
 			if ($conn->query($query) === TRUE) {
-				echo "New record created successfully\n";
+				$desc = addslashes($_POST['description']);
+				$qtable_0 = "";
+				$qtable_1 = "";
+				$index = 0;
+				foreach ($_POST as $key => $value) {
+					if ($index < count($table_config['table_0']) - 1) {
+						$qtable_0 .= $value;
+					} elseif ($index < count($table_config['table_0']) + count($table_config['table_1']) - 2) {
+						$qtable_1 .= $value;
+					}
+					$index++;
+				}
+
+				$query = "INSERT INTO entry (
+					table_0,
+					table_1,
+					description
+				)
+				VALUES (
+					'$qtable_0',
+					'$qtable_1',
+					'$desc'
+				)";
+
+				if ($conn->query($query) === TRUE) {
+					include("../site/entry_success_alert.php");
+				} else {
+					echo "Error: " . $query . "<br>" . $conn->error;
+				}
+			} else {
+				echo "Error creating table: " . $conn->error;
 			}
-			else {
-				echo "Error: " . $query . "<br>" . $conn->error;
-			}
+			$conn->close();
+
 		}
-		else {
-			echo "Error creating table: " . $conn->error;
-		}
-		$conn->close();
-	?>
+
+		?>
+
 </body>
+	<script type="text/javascript" src="../lib/jquery-3.6.0/jquery-3.6.0.slim.min.js"></script>
+	<script type="text/javascript" src="../lib/propper-2.9.2/propper.min.js"></script>
+	<script type="text/javascript" src="../lib/bootstrap-4.6.0/js/bootstrap.min.js"></script>
 </html>
---
+++ site/entry_success_alert.php
@@ -0,0 +1,26 @@
+<div class="container">
+	<center>
+		<h2>Hooray! Your entry was successfully saved.</h2>
+	</center>
+	<h5>It's as follows:</h5>
+	<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>id</th>
+					<th>entered_at</th>
+					<th>table_0</th>
+					<th>table_1</th>
+					<th>description</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td><?= "idk :)" ?></td>
+					<td><?= "Now" ?></td>
+					<td><?= $qtable_0 ?></td>
+					<td><?= $qtable_1 ?></td>
+					<td><?= $desc ?></td>
+				</tr>
+			</tbody>
+		</table>
+</div>
